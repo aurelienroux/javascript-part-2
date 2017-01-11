a1a7589e8c808a00ad5fe23cc5c25adb74323b44
Aurélien Roux <aureroux13@me.com>
2017-01-11 21:32:43 +0000

    warriors enter the arena

---
+++ warriors.js
@@ -0,0 +1,23 @@
+function Warrior(name, gender){
+    this.name = name,
+    this.gender = gender,
+    this.weapon = "wooden sword",
+    this.level = 1,
+    this.power = (Math.floor(Math.random() * 100) + 1),
+    this.fight = function (){
+        console.log(this.name + " rushes to the arena with a " + this.weapon )
+    },
+    this.faceoff = function(opponent){
+        if( this.power > opponent.power){
+            console.log(this.name + " did a fatality on " + opponent.name + "!!! BOOM!")
+        } else {
+           console.log(this.name + " got his a** kicked by " + opponent.name + "!!! BOOM!")
+        }
+    }
+}
+
+var conan = new Warrior("Conan", "M");
+var xena = new Warrior("Xena", "F");
+
+console.log(conan.fight());
+console.log(conan.faceoff(xena));
