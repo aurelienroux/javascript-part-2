aa57a8a3863750a76961b9cba342730a3e37300c
Aurélien Roux <aureroux13@me.com>
2017-01-12 00:11:31 +0000

    tiles array constructor working

--- map.js
+++ map.js
@@ -11,13 +11,28 @@ function Tile(type){
 
 function Map(width, height){
     this.width = width,
-    this.height = height
-    this.tiles = new Tile(function(){
+    this.height = height,
+    this.tilesMade = function(width, height){
+        var rows = [];
         for (var i = 0; i < width; i++) {
-            for(var j = 0; j < height; i++){
-                            
-            }
-        }
-    }    
-    )
+            var columns = [];
+            for (var j = 0; j < height; j++){
+                var rand = (Math.floor(Math.random() * 3) + 1)
+                var tmp = "grass";
+                if( rand === 1 ){
+                    tmp = "water"
+                } else if ( rand === 2 ){
+                    tmp = "sand"
+                } 
+                columns.push(new Tile(tmp));
+            }//first loop
+            rows.push(columns)
+        }//second loop
+        return rows;
+    },
+    this.tiles = this.tilesMade(width, height),
+    this.getWalkableOutput = "test"
 }
+
+var myMap = new Map(3,3);
+console.log(myMap.tiles[0][0]);
